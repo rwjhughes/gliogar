8a1655ba3532525c2970ab19c28bab4fb7344b6c
rwjhughes <richard@hughes.ie>
2023-10-18 15:04:08 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: 'https://www.eventbrite.ie/e/oiche-gliogar-tra-phaidin-the-brigideens-colm-seoighe-na-heasogai-tickets-733377310277?aff=oddtdtcreator',
+                permanent: false,
+            },
+        ]
+    }
+}
 
 module.exports = nextConfig

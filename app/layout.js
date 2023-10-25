ef69f3e75d44deff9319fc197cf4a6785a1bb5f8
rwjhughes <richard@hughes.ie>
2023-10-25 12:50:44 +0100

    remove html tag

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,6 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <body className={inter.className}>{children}</body>
   )
 }

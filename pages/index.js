d2ce13bfb1d91827af1345ea94caae785f1780f7
rwjhughes <richard@hughes.ie>
2024-06-19 01:26:43 +0100

    aadd

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,24 @@ import React from 'react';
 // ffmpeg -i 17.jpg -vf scale=400:500 17_0.jpg
 
 const Index = () => {
-  const numberOfImages = 18; //update on new posters
+
+  const numberOfImages = 19; // Update on new posters
   const tiles = [];
 
   for (let i = numberOfImages - 1; i >= 0; i--) {
-    tiles.push(
-      <div key={i} className={styles.tile}>
-        <img src={`/posters/${i}.jpg`} alt={`Image ${i}`} />
-      </div>
-    );
+    if (i === numberOfImages - 1) {
+      tiles.push(
+        <div key={i} className={`${styles.tile} ${styles.largeTile}`}>
+          <img src={`/posters/${i}.jpg`} alt={`Image ${i}`} />
+        </div>
+      );
+    } else {
+      tiles.push(
+        <div key={i} className={styles.tile}>
+          <img src={`/posters/${i}.jpg`} alt={`Image ${i}`} />
+        </div>
+      );
+    }
   }
 
   return (
@@ -22,21 +31,23 @@ const Index = () => {
       </div>
       <div className={styles.textcontainer}>
 
-        <div className={styles.content}>
+        <div className={` ${styles.content} ${styles.ticeid}`}>
           <div className={styles.title}>Ticéid
-            {/* <br />
-            <a href="https://www.tickettailor.com/events/gliogar/1193850" target="_blank"><li><date>28 Aibreán</date><br />Cuar & Muireann Ní Shé</li></a>
             <br />
-            <a href="https://www.tickettailor.com/events/gliogar/1209899" target="_blank"><li><date>2 Bealtaine</date> Brìghde Chaimbeul & Seamus Hyland</li></a> */}
+            <h2>Ar An Dé Deiridh</h2>
+            camchuairt mí Iúil
+            <a href="https://buytickets.at/gliogar/1293982" target="_blank"><li><date>16ú</date> - Inis Oírr - €5</li></a>
+            <a href="https://buytickets.at/gliogar/1293993" target="_blank"><li><date>18ú</date> - Indreabhán - €5</li></a>
+            <a href="https://buytickets.at/gliogar/1294006" target="_blank"><li><date>24ú</date> - Corr na Móna - saor in aisce</li></a>
           </div>
 
-          <div className={styles.tooltip}>diabhal ticéad anseo anois</div>
+          {/* <div className={styles.tooltip}>diabhal ticéad anseo anois</div> */}
         </div>
 
         <div className={styles.content}>
           <div className={styles.title}>Fúinn</div>
-          <div className={styles.tooltip} style={{ width: '330px' }}>Is cnuasach ealaíontóirí agus ceoltóirí muid Gliogar a chuireanns ceolchoirmeacha ar siúl,
-            go háirid trí mheáin na Gaeilge agus le ceoltóirí comhaimseartha Gaelacha.
+          <div className={styles.tooltip} style={{ width: '330px' }}>Is cnuasach ealaíontóirí muid Gliogar a chuireanns imeachtaí cultúrtha ar siúl,
+            go háirid trí mheáin na Gaeilge agus le ealaíontóirí comhaimseartha Gaelacha.
             Tá muid lonnaithe i gConamara Theas.<br /><br />
             📧 <a href="mailto:eolas@gliogar.ie" target="_blank">eolas@gliogar.ie</a><br /><br />
             <a href="https://instagram.com/gliogar_" target="_blank">@gliogar_</a></div>

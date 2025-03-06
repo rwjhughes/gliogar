04d3288b35823df431d00926577a1cfc2772358f
rwjhughes <richard@hughes.ie>
2025-03-06 23:33:12 +0000

    redirects

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
                 destination: 'https://daily.bandcamp.com/scene-report/ta-gliogar-ag-tabhairt-ardan-do-weirdo-ceol-ar-fud-na-heireann',
                 permanent: false,
             },
+            {
+                source: '/ultan',
+                destination: 'https://billetto.ie/e/ultan-o-brien-seoladh-albaim-dancing-the-line-amharclann-chois-fharraige-tickets-1189846',
+                permanent: false,
+            },
+            {
+                source: '/bds',
+                destination: 'https://tinyurl.com/smachtbhannai',
+                permanent: false,
+            },
         ]
     }
 }
